fdf767bc159af6b697a70b9e14dc0179fbb803ef
MSshizuka <1197940254@qq.com>
2019-12-25 14:26:21 +0800

    6.自定义组件

---
+++ components/my-props/my-props.js
@@ -0,0 +1,26 @@
+// components/my-props/my-props.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      value: '我是默认的标题'
+    }
+  },
+  externalClasses: ['titlecolor'],
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,25 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    count: 0,
+    message: {}
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  increment (event) {
+    console.log(event)
+    this.setData({
+      count: this.data.count + 1,
+      message: event.detail
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  itemclick(event) {
+    console.log(event.detail)
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  incrementCpn () {
+    const my_sel = this.selectComponent('#my-sel');
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    my_sel.increment(20)
   }
 })
